1bc56f6cee4c86d7ee1198ddb729e99dfe966fbf
rainyjune <rainyjune@live.cn>
2016-08-18 23:43:13 +0800

    Update files

--- image-upload.js
+++ image-upload.js
@@ -4,6 +4,7 @@ class YuanImageUpload {
     let fileControlId = uploadButton.getAttribute('data-for'),
         previewContainerId = uploadButton.getAttribute('data-preview');
     this.fileControl = document.getElementById(fileControlId);
+    // The div output where the content will be displayed.
     this.previewContainer = document.getElementById(previewContainerId);
     
     this.addEventListeners();
@@ -26,23 +27,31 @@ class YuanImageUpload {
   handleFiles() {
     let files = this.fileControl.files,
         fileLength = files.length;
-    debugger;
-    for (var i = 0; i < fileLength; i++) {
-      var file = files[i];
-      var imageType = /^image\//;
+        
+    for (let i = 0; i < fileLength; i++) {
+      let file = files[i];
+      let imageType = /^image\//;
       
       if (!imageType.test(file.type)) {
         continue;
       }
-      
-      var img = document.createElement("img");
-      //img.classList.add("obj");
-      img.file = file;
-      this.previewContainer.appendChild(img); // Assuming that "preview" is the div output where the content will be displayed.
-      
-      var reader = new FileReader();
-      reader.onload = (function(aImg) { return function(e) { aImg.src = e.target.result; }; })(img);
-      reader.readAsDataURL(file);
+      this.createImgContainer(file);
     }
+  }
+  
+  createImgContainer(file) {
+    let container = document.createElement("div");
+    container.classList.add('imgContainer');
+    
+    let img = document.createElement("img");
+    img.classList.add("thumbnail");
+    img.file = file;
+    
+    let reader = new FileReader();
+    reader.onload = (function(aImg) { return function(e) { aImg.src = e.target.result; }; })(img);
+    reader.readAsDataURL(file);
+    
+    container.appendChild(img);
+    this.previewContainer.appendChild(container); 
   }
 }
