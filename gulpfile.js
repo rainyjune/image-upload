cbe9c71ffd1b737e140f03637dfccdc120d1b5a3
rainyjune <rainyjune@live.cn>
2016-08-19 14:43:20 +0800

    Use gulp-csso

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 var gulp = require("gulp");
 var uglify = require('gulp-uglify');
 var babel = require("gulp-babel");
+var csso = require('gulp-csso');
 
 gulp.task("default", function () {
   return gulp.src("image-upload.js")
     .pipe(babel())
     .pipe(uglify())
     .pipe(gulp.dest("dist"));
+});
+
+gulp.task('cssmin', function () {
+    return gulp.src('image-upload.css')
+        .pipe(csso())
+        .pipe(gulp.dest('dist'));
 });
